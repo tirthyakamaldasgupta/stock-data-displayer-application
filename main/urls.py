ef3629ec7085f559ab4474d8b0b1bd7ddbbe65ef
tirthyakamaldasgupta <dasguptatirthyakamal>
2022-11-02 22:37:21 +0530

    Added an endpoint to query the historical data of any company

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, get_yahoo_finance_historical_data
 
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('<str:company_name>', get_yahoo_finance_historical_data, name='get_yahoo_finance_historical_data')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import yfinance as yahoo_finance
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+@api_view(['GET'])
+def get_yahoo_finance_historical_data(request, company_name: str):
+    company = yahoo_finance.Ticker(company_name)
+
+    historical_data = company.history()
+
+    if not historical_data.empty:
+        return Response(
+            {
+                "result": historical_data.T.to_dict().values()
+            },
+            status.HTTP_200_OK
+        )
+    return Response(
+        {
+            "result": []
+        },
+        status.HTTP_404_NOT_FOUND
+    )
