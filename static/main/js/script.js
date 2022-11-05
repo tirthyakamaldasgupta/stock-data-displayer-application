35034fbaf2fc0e6781dff9efffea932724844deb
tirthyakamaldasgupta <dasguptatirthyakamal>
2022-11-05 20:42:03 +0530

    Made minor improvements in the frontend. The websocket functionality has been temporarily disabled.

--- static/main/js/script.js
+++ static/main/js/script.js
@@ -1,7 +1,11 @@
 function display_historical_data() {
 
     const company_name = $("#company-search-input").val();
-    
+
+    $("#company-search-input").val("");
+
+    $("#information-fetch-error-span").text("");
+    $("#historical-data-fetch-error-span").text("");
     $("#information-container-table-label").text("");
     $("#historical-data-container-table-label").text("");
     $("#information-container-table").find("thead").empty();
@@ -10,15 +14,17 @@ function display_historical_data() {
     $("#historical-data-container-table").find("tbody").empty();
 
     if (company_name != '') {
+        $("#information-container-table-label").text("Information");
+        $("#historical-data-container-table-label").text("Historical data");
+
         $.ajax({
             type: "GET",
             url: "http://127.0.0.1:8000/" + company_name + "/information",
             contentType: "application/json",
             success: function (data) {
                 if (data["information"]) {
-                    $("#information-container-table-label").text("Information");
-                    $("#historical-data-container-table-label").text("Historical data")
-                    
+                    $("#company-search-input").val("");
+
                     $("#information-container-table").find("thead").append(
                         "<tr>" +
                         "<th scope=\"col\">Company display name</th>" +
@@ -33,68 +39,57 @@ function display_historical_data() {
                         "</tr>"
                     )
 
-                    const company_information_url = "ws://localhost:8000/ws/v1/" + "information"
+                    // const company_information_url = "ws://localhost:8000/ws/v1/" + "information"
 
-                    var socket = new WebSocket(company_information_url)
+                    // var socket = new WebSocket(company_information_url)
                     
-                    socket.onmessage = function(event) {
-                        var data = JSON.parse(event.data);
+                    // socket.onmessage = function(event) {
+                    //     var data = JSON.parse(event.data);
 
-                        $("#regular-market-price-data").text(data["information"]["regular_market_price"]);
-                    }
+                    //     $("#regular-market-price-data").text(data["information"]["regular_market_price"]);
+                    // }
+                }
+            },
+            error: function (data) {
+                $("#information-fetch-error-span").text("Either no infomation about the company was found or you are not connected to the internet!");
+            }
+        });
 
-                    $.ajax({
-                        type: "GET",
-                        url: "http://127.0.0.1:8000/" + company_name + "/historical-data",
-                        contentType: "application/json",
-                        success: function (data) {
-                            if (data["historical_data"].length > 0) {
-                                $("#historical-data-container-table").find("thead").append(
-                                    "<tr>" +
-                                    "<th scope=\"col\">Open</th>" +
-                                    "<th scope=\"col\">High</th>" +
-                                    "<th scope=\"col\">Low</th>" +
-                                    "<th scope=\"col\">Close</th>" +
-                                    "<th scope=\"col\">Volume</th>" +
-                                    "<th scope=\"col\">Dividends</th>" +
-                                    "<th scope=\"col\">Stock Splits</th>" +
-                                    "</tr>"
-                                );
-            
-                                $.each(data["historical_data"], function (index, item) {
-                                    $("#historical-data-container-table").find("tbody").append(
-                                        "<tr>" +
-                                        "<td>" + item["Open"] + "</td>" +
-                                        "<td>" + item["High"] + "</td>" +
-                                        "<td>" + item["Low"] + "</td>" +
-                                        "<td>" + item["Close"] + "</td>" +
-                                        "<td>" + item["Volume"] + "</td>" +
-                                        "<td>" + item["Dividends"] + "</td>" +
-                                        "<td>" + item["Stock Splits"] + "</td>" +
-                                        "</tr>"
-                                    )
-                                });
-            
-                                $("#company-search-input").val("");
-                            }
-                        },
-                        error: function (data) {
-                            console.log("No results found");
-            
-                            $("#company-search-input").val("");
-                        }
+        $.ajax({
+            type: "GET",
+            url: "http://127.0.0.1:8000/" + company_name + "/historical-data",
+            contentType: "application/json",
+            success: function (data) {
+                if (data["historical_data"].length > 0) {
+                    $("#historical-data-container-table").find("thead").append(
+                        "<tr>" +
+                        "<th scope=\"col\">Open</th>" +
+                        "<th scope=\"col\">High</th>" +
+                        "<th scope=\"col\">Low</th>" +
+                        "<th scope=\"col\">Close</th>" +
+                        "<th scope=\"col\">Volume</th>" +
+                        "<th scope=\"col\">Dividends</th>" +
+                        "<th scope=\"col\">Stock Splits</th>" +
+                        "</tr>"
+                    );
+
+                    $.each(data["historical_data"], function (index, item) {
+                        $("#historical-data-container-table").find("tbody").append(
+                            "<tr>" +
+                            "<td>" + item["Open"] + "</td>" +
+                            "<td>" + item["High"] + "</td>" +
+                            "<td>" + item["Low"] + "</td>" +
+                            "<td>" + item["Close"] + "</td>" +
+                            "<td>" + item["Volume"] + "</td>" +
+                            "<td>" + item["Dividends"] + "</td>" +
+                            "<td>" + item["Stock Splits"] + "</td>" +
+                            "</tr>"
+                        )
                     });
                 }
             },
             error: function (data) {
-                $("#error-modal").find("p").text("Either no infomation about the company was found or you are not connected to the internet!");
-                $("#error-modal").fadeIn();
-
-                setTimeout(function () {
-                    $("#error-modal").fadeOut();
-                }, 5000);
-
-                console.log("No results found");
+                $("#historical-data-fetch-error-span").text("Either no historical data about the company was found or you are not connected to the internet!");
             }
         });
     }
